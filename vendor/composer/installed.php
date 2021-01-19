d0a5d3025cb0af1d367a1ef8c8327f52b9316b7c
sawkea <shoareau@orange.fr>
2021-01-19 19:37:41 +0100

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dbedd8ccecd95059eafd7dad7d0ca8b5070c7e5a',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dbedd8ccecd95059eafd7dad7d0ca8b5070c7e5a',
+    ),
+  ),
+);
