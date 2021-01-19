57c43c3f369a8feee080ba6e25da43002d0320df
sawkea <shoareau@orange.fr>
2021-01-19 19:46:57 +0100

    Updates composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,19 +6,19 @@
     'aliases' => 
     array (
     ),
-    'reference' => 'dbedd8ccecd95059eafd7dad7d0ca8b5070c7e5a',
-    'name' => '__root__',
+    'reference' => 'd0a5d3025cb0af1d367a1ef8c8327f52b9316b7c',
+    'name' => 'shoar/harvestdata',
   ),
   'versions' => 
   array (
-    '__root__' => 
+    'shoar/harvestdata' => 
     array (
       'pretty_version' => 'dev-master',
       'version' => 'dev-master',
       'aliases' => 
       array (
       ),
-      'reference' => 'dbedd8ccecd95059eafd7dad7d0ca8b5070c7e5a',
+      'reference' => 'd0a5d3025cb0af1d367a1ef8c8327f52b9316b7c',
     ),
   ),
 );
