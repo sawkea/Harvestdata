c770d743e9a56cbc3ae4566eaadab83356b25a1f
sawkea <shoareau@orange.fr>
2021-01-22 17:57:26 +0100

    Mise en place du router

--- public/index.php
+++ public/index.php
@@ -1,42 +1,14 @@
 <?php
 
-echo "test";
-// require('controller/controller.php');
+require('../vendor/autoload.php');
 
-// try { // On essaie de faire des choses
-//     if (isset($_GET['action'])) {
-//         if ($_GET['action'] == 'listPosts') {
-//             listPosts();
-//         }
-//         elseif ($_GET['action'] == 'post') {
-//             if (isset($_GET['id']) && $_GET['id'] > 0) {
-//                 post();
-//             }
-//             else {
-//                 // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
-//                 throw new Exception('Aucun identifiant de billet envoyé');
-//             }
-//         }
-//         elseif ($_GET['action'] == 'addComment') {
-//             if (isset($_GET['id']) && $_GET['id'] > 0) {
-//                 if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-//                     addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-//                 }
-//                 else {
-//                     // Autre exception
-//                     throw new Exception('Tous les champs ne sont pas remplis !');
-//                 }
-//             }
-//             else {
-//                 // Autre exception
-//                 throw new Exception('Aucun identifiant de billet envoyé');
-//             }
-//         }
-//     }
-//     else {
-//         listPosts();
-//     }
-// }
-// catch(Exception $e) { // S'il y a eu une erreur, alors...
-//     echo 'Erreur : ' . $e->getMessage();
-// }
+$router = new App\Router\Router($_GET['url']);
+
+// Différentes routes 
+$router->get('/', function(){ echo 'Home page'; });
+$router->get('/posts', function(){ echo 'Tous les articles'; });
+$router->get('/posts/:id', function($id){ echo 'Afficher l\'article' . $id; });
+$router->post('/posts/:id', function($id){ echo 'Poster l\'article' . $id; });
+
+
+$router->run();
