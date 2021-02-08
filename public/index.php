1f12ba52e6e8ddc167db85d3aeeb0ddc277c9dda
sawkea <shoareau@orange.fr>
2021-02-08 10:09:10 +0100

    Install twig avec le routeur

--- public/index.php
+++ public/index.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Twig;
+
 require('../vendor/autoload.php');
 
 $router = new App\Router\Router($_GET['url']);
 
 // Différentes routes 
-$router->get('/', function(){ echo 'Home page'; });
+$router->get('/', function(){ 
+   
+    $twig = new Twig('base.html.twig');
+    $twig->render([   
+            
+    ]); 
+});
 $router->get('/posts', function(){ echo 'Tous les articles'; });
 $router->get('/posts/:id', function($id){ echo 'Afficher l\'article' . $id; });
 $router->post('/posts/:id', function($id){ echo 'Poster l\'article' . $id; });
